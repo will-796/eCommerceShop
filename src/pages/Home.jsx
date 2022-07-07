fec3f8f9d483fc56275ebfdf70292f310266396f
Debora Franchi <deborafranchicarvalho@gmail.com>
2022-07-07 22:18:26 +0200

    req2

---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      inputSearch: '',
+    };
+  }
+
+  render() {
+    const { inputSearch } = this.state;
+    return (
+
+      <header>
+        <input type="text" />
+      </header>
+      {inputSearch.length < 1 ? <h2>Digite algum termo de pesquisa ou escolha uma categoria</h2> : <div></div> } 
+      
+    );
+  }
+}
