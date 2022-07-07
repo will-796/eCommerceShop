6688d009efa9224e686bcbb9f968025ba22cb6c1
willian portela <willianportela1@gmail.com>
2022-07-07 17:47:20 -0300

    finalizando requisito 2 passando em todos os testes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,15 +8,29 @@ export default class Home extends Component {
     };
   }
 
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  };
+
   render() {
     const { inputSearch } = this.state;
     return (
-
-      <header>
-        <input type="text" />
-      </header>
-      // { inputSearch.length < 1 ? <h2>Digite algum termo de pesquisa ou escolha uma categoria</h2> : <div></div> }
-
+      <div>
+        <header>
+          <input
+            type="text"
+            name="inputSearch"
+            value={ inputSearch }
+            onChange={ this.handleChange }
+          />
+        </header>
+        {inputSearch.length === 0 && (
+          <div data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </div>
+        )}
+      </div>
     );
   }
 }
---
+++ src/pages/ProductDetail.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class ProductDetail extends Component {
+  render() {
+    return (
+      <div>ProductDetail</div>
+    );
+  }
+}
