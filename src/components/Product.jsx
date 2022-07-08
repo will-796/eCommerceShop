39ac1cbf11538766c71b62970700c0c62cb6f690
willian portela <willianportela1@gmail.com>
2022-07-08 15:25:30 -0300

    arrumando requisito 5 e aplicando alguma coisa de css legal e feia

---
+++ src/components/Product.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Product extends Component {
+  render() {
+    const { product } = this.props;
+    return (
+      <div data-testid="product" className="product">
+        <p>{product.title}</p>
+        <img src={ product.thumbnail } alt="" />
+        <p>{product.price}</p>
+        <Link
+          data-testid="product-detail-link"
+          to={ `/productDetail/${product.id}` }
+        >
+          detalhes do produto
+
+        </Link>
+      </div>
+    );
+  }
+}
+
+Product.propTypes = {
+  product: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    price: PropTypes.string.isRequired,
+    thumbnail: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+  }).isRequired,
+};
