9ddc91537efd393fd14739a9218f7fe424450dbc
willian portela <willianportela1@gmail.com>
2022-07-08 10:19:40 -0300

    arrumando lint

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import { getCategories } from '../services/api';
 import { Link } from 'react-router-dom';
-
+import { getCategories } from '../services/api';
 
 export default class Home extends Component {
   constructor() {
