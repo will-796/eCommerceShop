94f707a0eeae66907681992160f91fcdd9b812e9
willian portela <willianportela1@gmail.com>
2022-07-11 10:47:25 -0300

    fazendo parte do requisito 10 na branch 11 pq sim

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ShoppingCartProdutcs from '../components/ShoppingCartProdutcs';
 import { getProductData } from '../services/api';
-import { recoveryFromSection } from '../services/sessionStorage';
+import { handleSubmit, recoveryFromSection } from '../services/sessionStorage';
 
 class ShoppingCart extends React.Component {
   constructor() {
     super();
     this.state = {
       data: [],
-
+      unityProducts: [],
     };
   }
 
@@ -22,29 +22,62 @@ class ShoppingCart extends React.Component {
     const array = await Promise.all(result);
     this.setState({
       data: array,
-      totalPrice: array.reduce((totalprice, product) => totalprice + product.price, 0),
+      totalPrice: array.reduce(
+        (totalprice, product) => totalprice + product.price,
+        0,
+      ),
+      unityProducts: array.filter(
+        ({ id }, index) => !idProducts.includes(id, index + 1),
+      ),
     });
-  }
+  };
+
+  handleAddButtonClick = async (id) => {
+    const idProducts = recoveryFromSection();
+    const newIdProducts = [...idProducts, id];
+    handleSubmit(id);
+    const result = newIdProducts.map((ids) => getProductData(ids));
+    const array = await Promise.all(result);
+    this.setState({
+      data: array,
+      totalPrice: array.reduce(
+        (totalprice, product) => totalprice + product.price,
+        0,
+      ),
+    });
+  };
+
+  handleRemoveButtonClick = () => {};
 
   render() {
-    const { data, totalPrice } = this.state;
+    const { data, totalPrice, unityProducts } = this.state;
     console.log(data);
+    console.log(unityProducts);
     return (
       <div>
         <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
-        {data.map((item, index) => (
+        {unityProducts.map((item, index) => (
           <div key={ index }>
-            <h1 data-testid="shopping-cart-product-name">{ item.title }</h1>
+            <h1 data-testid="shopping-cart-product-name">{item.title}</h1>
             <h3>{item.price}</h3>
-            <h3 data-testid="shopping-cart-product-quantity">1</h3>
-            {' '}
+            <h3 data-testid="shopping-cart-product-quantity">
+              <button
+                type="button"
+                onClick={ () => this.handleRemoveButtonClick(item.id) }
+              >
+                Remove 1 produto
+              </button>
+              {data.filter(({ id }) => id === item.id).length}
+              <button
+                type="button"
+                onClick={ () => this.handleAddButtonClick(item.id) }
+              >
+                Adiciona 1 produto
+              </button>
+            </h3>
           </div>
         ))}
-        <div>
-          {
-            totalPrice
-          }
-        </div>
+        <div>{totalPrice}</div>
         <ShoppingCartProdutcs />
       </div>
     );
